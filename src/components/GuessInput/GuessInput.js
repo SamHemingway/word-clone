e059e9ae0ef3c4da06142863feb39a8d09f46eef
Sam Hemingway <65196819+SamHemingway@users.noreply.github.com>
2023-02-01 12:14:44 -0500

    Exercise 1 completed
    Commit before checking the solution.

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function GuessInput() {
+  const [guessInProgress, setGuessInProgress] = React.useState("");
+  const [finishedGuess, setFinishedGuess] = React.useState({
+    guess: undefined,
+  });
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (guessInProgress.length < 5) {
+          console.error("You need to enter at least 5 letters!");
+          return;
+        }
+        const nextFinishedGuess = { guess: guessInProgress };
+        setFinishedGuess(nextFinishedGuess);
+        console.log(nextFinishedGuess);
+        setGuessInProgress("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guessInProgress}
+        maxLength="5"
+        onChange={(event) => {
+          setGuessInProgress(event.target.value.toUpperCase());
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
